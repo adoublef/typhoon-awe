129bd0eaa7df5e2eb0454bfa445a6ad4621786e8
Kristopher Rahim Afful-Brown <kristopherab@gmail.com>
2023-09-24 20:50:13 +0100

    udpate iam service

--- iam/deps.ts
+++ iam/deps.ts
@@ -2,3 +2,5 @@ export { createGitHubOAuthConfig, } from "$deno_kv_oauth/lib/create_github_oauth
 export { signIn } from "$deno_kv_oauth/lib/sign_in.ts";
 export { signOut } from "$deno_kv_oauth/lib/sign_out.ts";
 export { handleCallback as callback } from "$deno_kv_oauth/lib/handle_callback.ts";
+export { getSessionId } from "$deno_kv_oauth/lib/get_session_id.ts";
+export type { OAuth2ClientConfig } from "$deno_kv_oauth/deps.ts";
--- iam/service.ts
+++ iam/service.ts
@@ -2,9 +2,23 @@ import { Hono } from "~/deps.ts";
 import { handleCallback } from "~/iam/routes/handle_callback.ts";
 import { handleSignIn } from "~/iam/routes/handle_sign_in.ts";
 import { handleSignOut } from "~/iam/routes/handle_sign_out.ts";
+import { handleIndex } from "~/iam/routes/handle_index.tsx";
 
+/**
+ * - `GET /`
+ * 
+ * - `GET /signup`
+ * 
+ * - `GET /signout`
+ * 
+ * - `GET /callback`
+ * 
+ * - `GET /settings`
+ */
 export const app = new Hono();
 
+app.get("/", handleIndex());
 app.get("/signin", handleSignIn());
 app.get("/callback", handleCallback());
 app.get("/signout", handleSignOut());
+// app.get("/settings", handleSettings())
