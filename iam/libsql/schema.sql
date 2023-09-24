fc89f4be039a23f7c29561444f8a69ff24704c88
Kristopher Rahim Afful-Brown <kristopherab@gmail.com>
2023-09-24 15:44:56 +0100

    add schema

---
+++ iam/libsql/schema.sql
@@ -0,0 +1,16 @@
+CREATE TABLE `profiles` (
+    `id` TEXT NOT NULL,
+    `display` TEXT NOT NULL UNIQUE,
+    `name` TEXT,
+    `image` TEXT,
+    --
+    PRIMARY KEY (`id`)
+);
+
+CREATE TABLE `credentials` (
+    `profile` TEXT NOT NULL,
+    `login` TEXT NOT NULL UNIQUE,
+    -- 
+    FOREIGN KEY (`profile`) REFERENCES `profiles` (`id`),
+    PRIMARY KEY (`profile`, `login`) 
+);
