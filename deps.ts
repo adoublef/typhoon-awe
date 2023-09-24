24a529c2d217c432a52040afb58edbf4142e91da
Kristopher Rahim Afful-Brown <kristopherab@gmail.com>
2023-09-24 17:28:40 +0100

    update deps

--- deps.ts
+++ deps.ts
@@ -5,3 +5,14 @@ export { serveStatic } from "$hono/adapter/deno/serve-static.ts";
 export { logger } from "$hono/middleware/logger/index.ts";
 export type { HtmlEscapedString } from "$hono/utils/html.ts";
 export { html } from "$hono/helper/html/index.ts";
+/* libsql */
+export type { Client } from "$libsql-client-ts/api.js";
+/* valibot */
+export { parse } from "$valibot/src/methods/parse/parse.ts";
+export { transform } from "$valibot/src/methods/transform/transform.ts";
+export { object } from "$valibot/src/schemas/object/object.ts";
+export { string } from "$valibot/src/schemas/string/string.ts";
+export type { Output } from "$valibot/src/types.ts";
+export { optional } from "$valibot/src/schemas/optional/optional.ts";
+export { nullable } from "$valibot/src/schemas/nullable/nullable.ts";
+export { nullish } from "$valibot/src/schemas/nullish/nullish.ts";
