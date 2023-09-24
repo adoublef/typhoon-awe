0f18b4d94892899b3257ca98ea81f467167906c5
Kristopher Rahim Afful-Brown <kristopherab@gmail.com>
2023-09-24 22:26:45 +0100

    update to main entry

--- cmd/typhoon-awe/main.ts
+++ cmd/typhoon-awe/main.ts
@@ -1,11 +1,32 @@
-import { Hono, logger, serveStatic } from "~/deps.ts";
-import { handleIndex } from "../../iam/routes/handle_index.tsx";
+import { Client, Hono, logger, serveStatic } from "~/deps.ts";
+import { app as iam } from "~/iam/service.ts";
+import { createClient } from "~/lib/libsql/deno.ts";
+import { getRequiredEnv } from "~/lib/env.ts";
+import { oauthClient as github } from "~/iam/github/oauth_client.ts";
+import { oauth, session } from "~/iam/middleware.ts";
+import { denoKv, turso } from "~/middleware.ts";
+
+const sql = `SELECT name FROM sqlite_master WHERE type = 'table' AND name = ?`;
+
+async function ping(c: Client, tableName: string): Promise<boolean> {
+    return (await c.execute({ sql, args: [tableName] })).rows.length !== 0;
+}
 
 if (import.meta.main) {
+    const [url, authToken] = ["DATABASE_URL", "TURSO_TOKEN"].map(getRequiredEnv);
+    const db = createClient({ url, authToken });
+    if (
+        !(await ping(db, "profiles"))
+    ) {
+        throw new Error("failed to connect to the database");
+    }
+
+    const kv = await Deno.openKv();
+
     const app = new Hono();
-    app.use("*", logger());
+    app.use("*", logger(), session(), denoKv(kv), turso(db), oauth(github));
     {
-        app.get("/", handleIndex());
+        app.route("/", iam);
     }
     app.get("/static/*", serveStatic({ root: "./" }));
 
