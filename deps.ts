a07cda5baa7df3dbf7a4150d54be09a3453e3a41
Kristopher Rahim Afful-Brown <kristopherab@gmail.com>
2023-09-24 14:53:20 +0100

    update deps with htmx

--- deps.ts
+++ deps.ts
@@ -1,3 +1,7 @@
 /* hono */
 export { Hono } from "$hono/hono.ts";
+export type { Handler, MiddlewareHandler, ErrorHandler, NotFoundHandler } from "$hono/types.ts";
+export { serveStatic } from "$hono/adapter/deno/serve-static.ts";
 export { logger } from "$hono/middleware/logger/index.ts";
+export type { HtmlEscapedString } from "$hono/utils/html.ts";
+export { html } from "$hono/helper/html/index.ts";
