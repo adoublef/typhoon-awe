65350e76b986d01e92629c5fb5876bfffadd82b9
Kristopher Rahim Afful-Brown <kristopherab@gmail.com>
2023-09-25 21:35:49 +0100

    add control flow helpers

---
+++ jsx/control_flow/for.tsx
@@ -0,0 +1,17 @@
+import { html, HtmlEscapedString } from "~/deps.ts";
+
+/** [TODO](https://www.solidjs.com/docs/latest/api#for) */
+export function For<T, U extends HtmlEscapedString>({
+    each,
+    fallback,
+    children,
+}: ForProps<T, U>): HtmlEscapedString {
+    if (!each.length) return fallback || html``;
+    return html`${each.map(children)}`;
+}
+
+export type ForProps<T, U extends HtmlEscapedString = HtmlEscapedString> = {
+    each: readonly T[];
+    fallback?: HtmlEscapedString;
+    children: (item: T, index: number) => U;
+};
