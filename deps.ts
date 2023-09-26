18637522236eb2af050e1b996b3fc40e469597a5
Kristopher Rahim Afful-Brown <kristopherab@gmail.com>
2023-09-26 16:33:14 +0100

    update deps

--- deps.ts
+++ deps.ts
@@ -5,6 +5,15 @@ export { serveStatic } from "$hono/adapter/deno/serve-static.ts";
 export { logger } from "$hono/middleware/logger/index.ts";
 export type { HtmlEscapedString } from "$hono/utils/html.ts";
 export { html } from "$hono/helper/html/index.ts";
+export { Context } from "$hono/context.ts";
+export {
+    getCookie,
+    getSignedCookie,
+    setCookie,
+    setSignedCookie,
+    deleteCookie,
+} from "$hono/helper.ts";
+export type { CookieOptions } from "$hono/utils/cookie.ts";
 /* libsql */
 export type { Client } from "$libsql-client-ts/api.js";
 /* valibot */
@@ -12,8 +21,11 @@ export { parse } from "$valibot/src/methods/parse/parse.ts";
 export { transform } from "$valibot/src/methods/transform/transform.ts";
 export { object } from "$valibot/src/schemas/object/object.ts";
 export { string } from "$valibot/src/schemas/string/string.ts";
-export { } from "$valibot/src/schemas/number/number.ts";
+export { number } from "$valibot/src/schemas/number/number.ts";
+export { custom } from "$valibot/src/validations/custom/custom.ts";
 export type { Output } from "$valibot/src/types.ts";
 export { optional } from "$valibot/src/schemas/optional/optional.ts";
 export { nullable } from "$valibot/src/schemas/nullable/nullable.ts";
 export { nullish } from "$valibot/src/schemas/nullish/nullish.ts";
+/* std */
+export type { Cookie } from "$std/http/cookie.ts";
