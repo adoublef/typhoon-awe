20a207f46a852e9ae1926d730800a547a75ab8f3
Kristopher Rahim Afful-Brown <kristopherab@gmail.com>
2023-09-26 16:34:40 +0100

    move oauth helpers into 2 single files for better locality

--- iam/oauth/oauth_github_client.ts
+++
@@ -1,7 +0,0 @@
-import { createGitHubOAuthConfig } from "~/iam/deps.ts";
-import { getRequiredEnv } from "~/lib/env.ts";
-
-// https://docs.github.com/en/apps/oauth-apps/building-oauth-apps/authorizing-oauth-apps#redirect-urls
-export const oauthClient = createGitHubOAuthConfig({
-    redirectUri: `${getRequiredEnv("HOSTNAME")}/callback`,
-});
