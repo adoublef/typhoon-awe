83ffb6b0abcb542987698a37978aac1b66ff9e7f
Kristopher Rahim Afful-Brown <kristopherab@gmail.com>
2023-09-26 16:33:05 +0100

    update routes to use the new oauth switcher

--- iam/routes/handle_sign_in.ts
+++ iam/routes/handle_sign_in.ts
@@ -1,14 +1,21 @@
 import { Handler } from "~/deps.ts";
 import { signIn } from "~/iam/deps.ts";
 import { IamEnv } from "~/iam/middleware.ts";
+import { getOAuthClient } from "~/iam/oauth/get_oauth_client.ts";
+import { setClientCookie } from "~/iam/oauth/cookie.ts";
+import { parseProvider } from "~/iam/oauth/provider.ts";
 
 export function handleSignIn<
-    E extends IamEnv = IamEnv
+    E extends IamEnv
 >(): Handler<E> {
-    return async ({ redirect, req, get, header }) => {
-        const { headers } = await signIn(req.raw, get("iam"));
+    return async (c) => {
+        const clientName = parseProvider(c.req.query("v"));
+        const oauthClient = getOAuthClient(clientName);
 
-        header("set-cookie", headers.get("set-cookie")!);
-        return redirect(headers.get("location")!);
+        const { headers } = await signIn(c.req.raw, oauthClient);
+        setClientCookie(c, clientName);
+
+        c.header("set-cookie", headers.get("set-cookie")!, { append: true });
+        return c.redirect(headers.get("location")!);
     };
 }
--- iam/routes/handle_sign_out.ts
+++ iam/routes/handle_sign_out.ts
@@ -1,14 +1,14 @@
+import { Env } from "$hono/types.ts";
 import { Handler } from "~/deps.ts";
 import { signOut } from "~/iam/deps.ts";
-import { IamEnv } from "~/iam/middleware.ts";
 
 export function handleSignOut<
-    E extends IamEnv = IamEnv
+    E extends Env
 >(): Handler<E> {
     return async ({ redirect, req, header }) => {
         const { headers } = await signOut(req.raw);
         // signout use sessions(?)
-        header("set-cookie", headers.get("set-cookie")!);
+        header("set-cookie", headers.get("set-cookie")!, { append: true });
         return redirect("/",/* headers.get("location")! */);
     };
 }
