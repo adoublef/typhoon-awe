ea08afc57f3e52497822c494ca561e76f79c6f75
Kristopher Rahim Afful-Brown <kristopherab@gmail.com>
2023-09-26 12:12:21 +0100

    upadte deps

--- iam/deps.ts
+++ iam/deps.ts
@@ -1,4 +1,5 @@
-export { createGitHubOAuthConfig, } from "$deno_kv_oauth/lib/create_github_oauth_config.ts";
+export { createGitHubOAuthConfig } from "$deno_kv_oauth/lib/create_github_oauth_config.ts";
+export { createGoogleOAuthConfig } from "$deno_kv_oauth/lib/create_google_oauth_config.ts";
 export { signIn } from "$deno_kv_oauth/lib/sign_in.ts";
 export { signOut } from "$deno_kv_oauth/lib/sign_out.ts";
 export { handleCallback as callback } from "$deno_kv_oauth/lib/handle_callback.ts";
