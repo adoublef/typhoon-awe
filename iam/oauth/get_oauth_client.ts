fd38d944813bb4a482581bebe37250ce0d2df775
Kristopher Rahim Afful-Brown <kristopherab@gmail.com>
2023-09-26 16:32:24 +0100

    retrieve the client needed by provider name

---
+++ iam/oauth/get_oauth_client.ts
@@ -0,0 +1,22 @@
+import { OAuth2ClientConfig, createGitHubOAuthConfig, createGoogleOAuthConfig } from "~/iam/deps.ts";
+import { getRequiredEnv } from "~/lib/env.ts";
+import { Provider } from "~/iam/oauth/provider.ts";
+
+export function getOAuthClient(name: Provider): OAuth2ClientConfig {
+    switch (name) {
+        case "github": return github;
+        case "google": return google;
+    }
+}
+
+// https://docs.github.com/en/apps/oauth-apps/building-oauth-apps/authorizing-oauth-apps#redirect-urls
+export const github = createGitHubOAuthConfig({
+    redirectUri: `${getRequiredEnv("HOSTNAME")}/callback`,
+});
+
+// https://support.google.com/cloud/answer/6158849?hl=en#zippy=%2Cuser-consent%2Cauthorized-domains%2Cpublic-and-internal-applications
+// https://stackoverflow.com/a/67913727/4239443
+export const google = createGoogleOAuthConfig({
+    redirectUri: `${getRequiredEnv("HOSTNAME")}/callback`,
+    scope: getRequiredEnv("GOOGLE_OAUTH_SCOPE").split(",")
+});
