a9680b0f213af177142295939b1a6d9cd5e7d02c
Kristopher Rahim Afful-Brown <kristopherab@gmail.com>
2023-09-26 16:49:57 +0100

    update types

--- iam/routes/handle_sign_in.ts
+++ iam/routes/handle_sign_in.ts
@@ -1,13 +1,10 @@
 import { Handler } from "~/deps.ts";
 import { signIn } from "~/iam/deps.ts";
-import { IamEnv } from "~/iam/middleware.ts";
 import { getOAuthClient } from "~/iam/oauth/get_oauth_client.ts";
 import { setClientCookie } from "~/iam/oauth/cookie.ts";
 import { parseProvider } from "~/iam/oauth/provider.ts";
 
-export function handleSignIn<
-    E extends IamEnv
->(): Handler<E> {
+export function handleSignIn(): Handler {
     return async (c) => {
         const clientName = parseProvider(c.req.query("v"));
         const oauthClient = getOAuthClient(clientName);
