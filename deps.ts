bfcf1e75dbbc623013c5122c0aa872e5d2d76e5e
Kristopher Rahim Afful-Brown <kristopherab@gmail.com>
2023-10-05 20:54:43 +0100

    update deps

--- deps.ts
+++ deps.ts
@@ -1,31 +1,41 @@
 /* hono */
-export { Hono } from "$hono/hono.ts";
-export type { Handler, MiddlewareHandler, ErrorHandler, NotFoundHandler, Env, Variables } from "$hono/types.ts";
-export { serveStatic } from "$hono/adapter/deno/serve-static.ts";
+export { Hono, HTTPException } from "$hono/mod.ts";
+export type {
+    Handler,
+    MiddlewareHandler,
+    ErrorHandler,
+    NotFoundHandler,
+    Env,
+    Variables
+} from "$hono/types.ts";
 export { logger } from "$hono/middleware/logger/index.ts";
 export type { HtmlEscapedString } from "$hono/utils/html.ts";
 export { html } from "$hono/helper/html/index.ts";
 export { Context } from "$hono/context.ts";
 export {
-    getCookie,
-    getSignedCookie,
-    setCookie,
-    setSignedCookie,
-    deleteCookie,
+    getCookie
+    , getSignedCookie
+    , setCookie
+    , setSignedCookie
+    , deleteCookie
 } from "$hono/helper.ts";
 export type { CookieOptions } from "$hono/utils/cookie.ts";
 /* libsql */
-export type { Client } from "$libsql-client-ts/api.js";
+export { type Client, LibsqlError } from "$libsql/api";
 /* valibot */
-export { parse } from "$valibot/src/methods/parse/parse.ts";
-export { transform } from "$valibot/src/methods/transform/transform.ts";
-export { object } from "$valibot/src/schemas/object/object.ts";
-export { string } from "$valibot/src/schemas/string/string.ts";
-export { number } from "$valibot/src/schemas/number/number.ts";
-export { custom } from "$valibot/src/validations/custom/custom.ts";
-export type { Output } from "$valibot/src/types.ts";
-export { optional } from "$valibot/src/schemas/optional/optional.ts";
-export { nullable } from "$valibot/src/schemas/nullable/nullable.ts";
-export { nullish } from "$valibot/src/schemas/nullish/nullish.ts";
+export {
+    parse
+    , transform
+    , object
+    , string
+    , number
+    , custom
+    , type Output
+    , optional
+    , nullable
+    , nullish
+    , maxBytes
+} from "$valibot/mod.ts";
 /* std */
 export type { Cookie } from "$std/http/cookie.ts";
+export { Status } from "$std/http/http_status.ts"; 
