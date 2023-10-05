1943a32d4907a5583c0c861ced0b76e6f3c6d529
Kristopher Rahim Afful-Brown <kristopherab@gmail.com>
2023-10-05 21:39:55 +0100

    check tests work

--- iam/iam_test.ts
+++ iam/iam_test.ts
@@ -3,24 +3,23 @@ import { assertEquals } from "$std/assert/assert_equals.ts";
 import { parseCredentials, parseProfile } from "~/iam/iam.ts";
 import { Ulid } from "~/lib/id/ulid.ts";
 
-Deno.test("parse_profile()", async (test) => {
-    await test.step("valid input data", (test) => {
-        const input = {
-            display: "adoublef",
-            name: "Kristopher Rahim",
-        };
-        const output = parseProfile(input);
-        assertExists(output.id);
+Deno.test("iam", async (test) => {
+    await test.step("parse_profile()", async (test) => {
+        await test.step("valid input data", (test) => {
+            const input = { display: "adoublef", name: "Kristopher Rahim", };
+            const output = parseProfile(input);
+            assertExists(output.id);
+        });
     });
-});
 
-Deno.test("parse_credentials()", async (test) => {
-    await test.step("valid input data", () => {
-        const input = {
-            profile: new Ulid().toString(),
-            login: "passw|1234567890",
-        };
-        const output = parseCredentials(input);
-        assertEquals(input.profile, output.profile.toString());
+    await test.step("parse_credentials()", async (test) => {
+        await test.step("valid input data", () => {
+            const input = {
+                profile: new Ulid().toString(),
+                login: "passw|1234567890",
+            };
+            const output = parseCredentials(input);
+            assertEquals(input.profile, output.profile.toString());
+        });
     });
 });
