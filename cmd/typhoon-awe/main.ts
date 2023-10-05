b2bacc410c8b0f7e9c0636263af7c5d610994444
Kristopher Rahim Afful-Brown <kristopherab@gmail.com>
2023-10-05 22:59:58 +0100

    atlas gh action

--- cmd/typhoon-awe/main.ts
+++ cmd/typhoon-awe/main.ts
@@ -1,18 +1,15 @@
 import { Hono, logger, createClient } from "~/deps.ts";
 import { env } from "~/lib/env.ts";
-import { session } from "~/iam/middleware.ts";
+import { app as iam } from "~/iam/service.ts";
 import { denoKv } from "~/lib/kv/deno_kv.ts";
 import { ping, turso } from "~/lib/turso.ts";
 import { serve } from "~/lib/serve.ts";
 import { handleAbout } from "./handle_about.tsx";
 import { handleError } from "./handle_error.tsx";
-import { handleIndex } from "./handle_index.tsx";
+import { session } from "~/iam/iam.ts";
 
 if (import.meta.main) {
-    const db = createClient({
-        url: env("DATABASE_URL"),
-        authToken: env("TURSO_TOKEN")
-    });
+    const db = createClient({ url: env("DATABASE_URL"), authToken: env("TURSO_TOKEN") });
     await ping(db);
 
     const kv = await Deno.openKv();
@@ -21,7 +18,7 @@ if (import.meta.main) {
         .use("*", logger(), session(), denoKv(kv), turso(db))
         .onError((_, c) => c.redirect("/ouch"));
     {
-        app.get("/", handleIndex());
+        app.route("/", iam);
         app.get("/about", handleAbout());
         app.get("/ouch", handleError());
     }
