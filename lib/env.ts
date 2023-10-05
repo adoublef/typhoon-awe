2b573e8b4ec8a59a9faf19e7b342c53dd15d3437
Kristopher Rahim Afful-Brown <kristopherab@gmail.com>
2023-10-05 21:19:27 +0100

    fix env to use deno

--- lib/env.ts
+++ lib/env.ts
@@ -1,7 +1,14 @@
-export function getRequiredEnv(key: string) {
-    const value = Deno.env.get(key);
+function unquote(value: string) {
+    return value.replace(/^['"]|['"]$/g, "");
+}
+
+/**
+ * Env is a helper to get environment variable 
+ */
+export function env(key: string): string {
+    const value = Deno.env.get(key)
     if (value === undefined) {
-        throw new Error(`"${key}" environment variable must be set`);
+        throw new ReferenceError(`"${key}" environment variable must be set`);
     }
-    return value;
+    return unquote(value);
 }
