810b55d2066ba7290739bfe73c5d29c0e2c2f61c
Kristopher Rahim Afful-Brown <kristopherab@gmail.com>
2023-10-07 18:45:08 +0100

    .

--- lib/turso.ts
+++ lib/turso.ts
@@ -24,9 +24,6 @@ export function turso<
  * Throws error if it failed the request.
  */
 export async function ping(c: Client): Promise<true> {
-    // this will currently only ping `SELECT 42`
-    // TODO -- ping for specific tables in the table
-    // TODO -- option to sync with the external database
     try {
         const ok = (await c.execute("SELECT 42")).rows.length !== 0;
         if (!ok) {
